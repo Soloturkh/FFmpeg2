2077461ed461d74d5c59e25653580abd5e761954
Soloturkh <127161170+Soloturkh@users.noreply.github.com>
2023-08-20 22:48:02 +0300

    Delete dashdec2.h

--- libavformat/dashdec2.h
+++
@@ -1,7 +0,0 @@
-typedef struct DASHContext {
-    // ... [Diğer değişkenler]
-
-    // Anahtar listesi için eklenen değişkenler
-    DecryptionKey decryption_keys[MAX_KEYS];
-    int num_keys;
-} DASHContext;
