e22a407e749b2064d026f86e3ea9527859346ccf
Soloturkh <127161170+Soloturkh@users.noreply.github.com>
2023-08-24 05:59:11 +0300

    Update dashdec.c

--- libavformat/dashdec.c
+++ libavformat/dashdec.c
@@ -723,7 +723,6 @@ static int resolve_content_path(AVFormatContext *s, const char *url, int *max_ur
     int size = 0;
     int i;
     int tmp_max_url_size = strlen(url);
-    av_log(s, AV_LOG_INFO, "İlk Url %s\n", url);
 
     for (i = n_baseurl_nodes-1; i >= 0 ; i--) {
         text = xmlNodeGetContent(baseurl_nodes[i]);
@@ -865,12 +864,8 @@ static int resolve_content_path(AVFormatContext *s, const char *url, int *max_ur
                 p = strchr(p, '/');
                 memset(p + 1, 0, strlen(p));
             }
-            av_log(s, AV_LOG_INFO, "text + start %s\n", text + start);
-            av_log(s, AV_LOG_INFO, "tmp_max_url_size %s\n", tmp_max_url_size);
             av_strlcat(tmp_str, text + start, tmp_max_url_size);
             xmlNodeSetContent(baseurl_nodes[i], tmp_str);
-            av_log(s, AV_LOG_INFO, "baseurl_nodes [i] %s\n", i);
-            av_log(s, AV_LOG_INFO, "tmp_str %s\n", tmp_str);
             updated = 1;
             xmlFree(text);
         }
